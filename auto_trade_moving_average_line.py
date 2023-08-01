8ef28d27f0a7450647ea6b03abe4b7dc05ab78f0
hogenius <hogeniuss@gmail.com>
2023-08-01 19:18:29 +0900

    ma_line work, config modify

--- auto_trade_moving_average_line.py
+++ auto_trade_moving_average_line.py
@@ -5,6 +5,11 @@ import pandas
 
 access = ""
 secret = ""
+coin_name = ""
+ma_1 = 0
+ma_2 = 0
+ma_3 = 0
+ma_check_sec = 0
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -22,28 +27,33 @@ with open('config.yaml') as f:
     config_data = yaml.load(f, Loader=yaml.FullLoader)
     access = config_data['key_access']
     secret = config_data['key_secret']
+    coin_name = config_data['coin_name']
+    ma_1 = config_data['ma_line_1']
+    ma_2 = config_data['ma_line_2']
+    ma_3 = config_data['ma_line_3']
+    ma_check_sec = config_data['ma_check_sec']
 
 # login
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
 while True:
-
+    
     try:
-        df = pyupbit.get_ohlcv("KRW-BTC", count=50)
-        df['10MA'] = df['close'].rolling(10).mean()
-        df['30MA'] = df['close'].rolling(30).mean()
-        df['50MA'] = df['close'].rolling(50).mean()
+        df = pyupbit.get_ohlcv(coin_name, count=ma_3)
+        df['MA_1'] = df['close'].rolling(ma_1).mean()
+        df['MA_2'] = df['close'].rolling(ma_2).mean()
+        df['MA_3'] = df['close'].rolling(ma_3).mean()
         pandas.set_option('display.float_format', lambda x: '%.1f' % x)
-        
+
         last_data = df.iloc[(len(df)-1)]
-        data_10ma = last_data['10MA']
-        data_30ma = last_data['30MA']
-        data_50ma = last_data['50MA']
+        data_ma_1 = last_data['MA_1']
+        data_ma_2 = last_data['MA_2']
+        data_ma_3 = last_data['MA_3']
 
-        print(f"10ma:{data_10ma}, 30ma:{data_30ma}, 50ma:{data_50ma}")
+        print(f"{ma_1}ma:{data_ma_1}, {ma_2}ma:{data_ma_2}, {ma_3}ma:{data_ma_3}")
 
-        if data_50ma < data_30ma < data_10ma:
+        if data_ma_3 < data_ma_2 < data_ma_1:
             #50일, 30일 , 10일 정배열이라면?
             print("order buy!!!")
             krw = get_balance("KRW")
@@ -51,7 +61,7 @@ while True:
                 buy_krw = krw*0.9995
                 #upbit.buy_market_order("KRW-BTC", buy_krw)
                 print(f"autotrade buy_market_order {buy_krw}")
-        elif data_10ma < data_30ma:
+        elif data_ma_1 <= data_ma_2:
             #10일 30일 이평선이 교차했다?
             print("order sell!!!")
             btc = get_balance("BTC")
@@ -62,7 +72,7 @@ while True:
         else:
             print("do nothing..")
 
-        time.sleep(60)
+        time.sleep(ma_check_sec)
     except Exception as e:
         print(e)
-        time.sleep(60)
+        time.sleep(ma_check_sec)
