4d80c2d2167c10875a0450912922a720f5f4bcd9
hogenius <hogeniuss@gmail.com>
2023-08-09 10:37:11 +0900

    vb_ma modify

--- auto_trade_vb_ma.py
+++ auto_trade_vb_ma.py
@@ -61,16 +61,18 @@ while True:
             df = pyupbit.get_ohlcv(config.coin_name, count=config.ma_3)
             df['MA_1'] = df['close'].rolling(config.ma_1).mean()
             df['MA_2'] = df['close'].rolling(config.ma_2).mean()
-            df['MA_3'] = df['close'].rolling(config.ma_3).mean()
+            #df['MA_3'] = df['close'].rolling(config.ma_3).mean()
             pandas.set_option('display.float_format', lambda x: '%.1f' % x)
 
             last_data = df.iloc[(len(df)-1)]
             data_ma_1 = last_data['MA_1']
             data_ma_2 = last_data['MA_2']
-            data_ma_3 = last_data['MA_3']
+            #data_ma_3 = last_data['MA_3']
 
-            print(f"{config.ma_1}ma:{data_ma_1}, {config.ma_2}ma:{data_ma_2}, {config.ma_3}ma:{data_ma_3}")
-            is_regulat_arr = (data_ma_3 < data_ma_2 < data_ma_1)
+            #print(f"{config.ma_1}ma:{data_ma_1}, {config.ma_2}ma:{data_ma_2}, {config.ma_3}ma:{data_ma_3}")
+            print(f"{config.ma_1}ma:{data_ma_1}, {config.ma_2}ma:{data_ma_2}")
+            #is_regulat_arr = (data_ma_3 < data_ma_2 < data_ma_1)
+            is_regulat_arr = (data_ma_2 < data_ma_1)
             print(f"is_regulat_arr : {is_regulat_arr}")
 
             target_price = get_target_price("KRW-BTC", best_k)
