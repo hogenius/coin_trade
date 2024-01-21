c03e7ea1c90efdbce504e51d40a775517be9ea0c
hogenius <hogeniuss@gmail.com>
2024-01-21 16:40:04 +0900

    손절/익절 rate 기능 추가.

--- auto_trade_vb_ma_multi.py
+++ auto_trade_vb_ma_multi.py
@@ -47,6 +47,22 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
+def get_revenue_rate(balances, ticker):
+    revenue_rate = 0.0
+
+    for coin in balances:
+        # 티커 형태로 전환
+        coin_ticker = coin['unit_currency'] + "-" + coin['currency']
+
+        if ticker == coin_ticker:
+            # 현재 시세
+            now_price = pyupbit.get_current_price(coin_ticker)
+             
+            # 수익률 계산을 위한 형 변환
+            revenue_rate = (now_price - float(coin['avg_buy_price'])) / float(coin['avg_buy_price']) * 100.0
+
+    return revenue_rate
+
 def print_msg(msg, withDiscord=True):
     if is_test:
         msg = 'TestMode:' + msg
@@ -60,9 +76,18 @@ def make_coin_list(list):
 
     for i in range(len(config.list_coin)):
         data = config.list_coin[i]
-        list.append({'name':data['name'], 'rate':data['rate'], 'best_k':find_best_k.GetBestK(data['name']),'is_buy':False, 'krw_avaiable':-1})
+        list.append({
+            'name':data['name'], 
+            'rate':data['rate'], 
+            'rate_profit':data['rate_profit'], 
+            'rate_stop_loss':data['rate_stop_loss'], 
+            'best_k':find_best_k.GetBestK(data['name']),
+            'is_buy':False, 
+            'is_sell':False, 
+            'krw_avaiable':-1})
     check_available_krw(list)
     print_msg(f"make_coin_list : {list}")
+
 
 def check_available_krw(list):
     #보유하고 있는 현금을 기준으로 비율을 정산합니다.
@@ -111,6 +136,7 @@ while True:
         check_available_krw(list_coin_info)
 
         is_need_refesh = False
+        balances = upbit.get_balances()
 
         for i in range(len(list_coin_info)):
 
@@ -122,54 +148,85 @@ while True:
             # 9시부터 < 현재 < 담날08:59:50까지 돌도록
             if start_time < now < end_time - datetime.timedelta(seconds=config.loop_sec*3):
 
-                #매수 프로세스
+                #매수 프로세스 체크 시작.
+
                 if list_coin_info[i]['is_buy'] == True:
+
+                    #매수도 했었고 그에 이어서 매도도 했었다면 더이상 할게 없다.
+                    if list_coin_info[i]['is_sell'] == True:
                         continue
 
-                #이동평균선을 구한다.
-                df = pyupbit.get_ohlcv(coin_name, count=config.ma_3)
-                df['MA_1'] = df['close'].rolling(config.ma_1).mean()
-                df['MA_2'] = df['close'].rolling(config.ma_2).mean()
-                #df['MA_3'] = df['close'].rolling(config.ma_3).mean()
-                pandas.set_option('display.float_format', lambda x: '%.1f' % x)
+                    rate_profit = list_coin_info[i]['rate_profit']
+                    rate_stop_loss = list_coin_info[i]['rate_stop_loss']
+                    if 0 < rate_profit or rate_stop_loss < 0:
 
-                last_data = df.iloc[(len(df)-1)]
-                data_ma_1 = last_data['MA_1']
-                data_ma_2 = last_data['MA_2']
-                #data_ma_3 = last_data['MA_3']
+                        #매수한 상태이니 이제 수익률을 계산합니다.
+                        revenue_rate = get_revenue_rate(balances, list_coin_info[i]['name'])
 
-                #print(f"{config.ma_1}ma:{data_ma_1}, {config.ma_2}ma:{data_ma_2}, {config.ma_3}ma:{data_ma_3}")
-                print(f"{config.ma_1}ma:{data_ma_1}, {config.ma_2}ma:{data_ma_2}")
-                #is_regulat_arr = (data_ma_3 < data_ma_2 < data_ma_1)
+                        is_need_sell = False
+                        if 0 < rate_profit and rate_profit <= revenue_rate:
+                            #익절하자!!
+                            is_need_sell = True
+                        elif rate_stop_loss < 0 and revenue_rate <= rate_stop_loss:
+                            #손절하자!!
+                            is_need_sell = True
 
-                #1번 이동평균이 2번 이동평균선보다 이상이면 정배열로 간주한다.
-                is_regulat_arr = (data_ma_2 < data_ma_1)
-                print(f"is_regulat_arr : {is_regulat_arr}")
+                        if is_need_sell:
+                            #전량 매도.
+                            coin_name_pure = coin_name.replace('KRW-', '')
+                            coin_balance = get_balance(coin_name_pure)
+                            if coin_balance > 0.00008:
+                                sell_coin = coin_balance*0.9995
+                                if is_test == False:
+                                    upbit.sell_market_order(coin_name, sell_coin)
+                                print_msg(f"autotrade sell_market_order {coin_name}:{sell_coin}")
+                                list_coin_info[i]['is_sell'] = True
 
-                target_price = get_target_price(coin_name, list_coin_info[i]['best_k'])
-                current_price = get_current_price(coin_name)
+                else:
+                    #이동평균선을 구한다.
+                    df = pyupbit.get_ohlcv(coin_name, count=config.ma_3)
+                    df['MA_1'] = df['close'].rolling(config.ma_1).mean()
+                    df['MA_2'] = df['close'].rolling(config.ma_2).mean()
+                    #df['MA_3'] = df['close'].rolling(config.ma_3).mean()
+                    pandas.set_option('display.float_format', lambda x: '%.1f' % x)
 
-                #이동평균선 정배열이면서 best_k에 의해 변동성이 돌파했다면?! 매수 가즈아
-                if is_regulat_arr and target_price < current_price:
-                    
-                    krw_total = get_balance("KRW")
-                    krw = list_coin_info[i]['krw_avaiable']
+                    last_data = df.iloc[(len(df)-1)]
+                    data_ma_1 = last_data['MA_1']
+                    data_ma_2 = last_data['MA_2']
+                    #data_ma_3 = last_data['MA_3']
 
-                    #보완처리. 비율료 계산되었던 현금가용액이 실제 보유현금액보다 크다?
-                    if krw_total < krw:
-                        krw = krw_total
+                    #print(f"{config.ma_1}ma:{data_ma_1}, {config.ma_2}ma:{data_ma_2}, {config.ma_3}ma:{data_ma_3}")
+                    print(f"{config.ma_1}ma:{data_ma_1}, {config.ma_2}ma:{data_ma_2}")
+                    #is_regulat_arr = (data_ma_3 < data_ma_2 < data_ma_1)
 
-                    if krw > 5000:
-                        buy_krw = krw*0.9995
-                        if is_test == False:
-                            upbit.buy_market_order(coin_name, buy_krw)
-                        print_msg(f"autotrade buy_market_order {coin_name}:{buy_krw}")
+                    #1번 이동평균이 2번 이동평균선보다 이상이면 정배열로 간주한다.
+                    is_regulat_arr = (data_ma_2 < data_ma_1)
+                    print(f"is_regulat_arr : {is_regulat_arr}")
 
-                        #매수한것으로 표기. 하루에 반복적으로 구매 하지 않습니다.
-                        #하루가 지나서 전량 매도가 되기전에 사용자 임의로 매도를 할수 있도록 말이죠.
-                        list_coin_info[i]['is_buy'] = True
+                    target_price = get_target_price(coin_name, list_coin_info[i]['best_k'])
+                    current_price = get_current_price(coin_name)
 
-                        #print(f"after buy list_coin_info : {list_coin_info}")
+                    #이동평균선 정배열이면서 best_k에 의해 변동성이 돌파했다면?! 매수 가즈아
+                    if is_regulat_arr and target_price < current_price:
+                        
+                        krw_total = get_balance("KRW")
+                        krw = list_coin_info[i]['krw_avaiable']
+
+                        #보완처리. 비율료 계산되었던 현금가용액이 실제 보유현금액보다 크다?
+                        if krw_total < krw:
+                            krw = krw_total
+
+                        if krw > 5000:
+                            buy_krw = krw*0.9995
+                            if is_test == False:
+                                upbit.buy_market_order(coin_name, buy_krw)
+                            print_msg(f"autotrade buy_market_order {coin_name}:{buy_krw}")
+
+                            #매수한것으로 표기. 하루에 반복적으로 구매 하지 않습니다.
+                            #하루가 지나서 전량 매도가 되기전에 사용자 임의로 매도를 할수 있도록 말이죠.
+                            list_coin_info[i]['is_buy'] = True
+
+                            #print(f"after buy list_coin_info : {list_coin_info}")
 
             else:
                 #전량 매도.
