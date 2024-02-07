9ab12eb305a6fd857786164037411bd2a70ee910
hogenius <hogeniuss@gmail.com>
2024-02-07 20:05:14 +0900

    find ticker

--- check_ticker.py
+++ check_ticker.py
@@ -9,52 +9,72 @@ from config import ConfigInfo
 from statistics import stdev
 config = ConfigInfo()
 is_test = True
-list_coin_info = []
-list_coin_info_warning = []
+list_ticker = []
+list_ticker_warning = []
+list_spike = []
 
-def CheckSpike():
+def MakeTickerList(listTicker, listTickerWarning):
+
+    listTicker.clear()
+    listTickerWarning.clear()
 
     tickers = pyupbit.get_tickers("KRW", True)
 
     for ticker in tickers:
         if 'CAUTION' in ticker['market_warning']:
-            list_coin_info_warning.append(ticker)
+            listTickerWarning.append(ticker)
             continue
 
-        list_coin_info.append(ticker)
+        listTicker.append(ticker)
 
-    print(list_coin_info)
-    print(f"list_coin_info count: {len(list_coin_info)}")
-    print(list_coin_info_warning)
-    print(f"list_coin_info_warning count: {len(list_coin_info_warning)}")
+    #print(list_ticker)
+    #print(f"list_coin_info count: {len(list_ticker)}")
+    #print(list_ticker_warning)
+    #print(f"list_coin_info_warning count: {len(list_ticker_warning)}")
 
 OUTLIER_COUNT = 5
 
-def CheckTicker(ticker):
+def CheckTicker(ticker, listTickerSpike):
 
-    df = pyupbit.get_ohlcv(ticker, interval="minutes3", count=200)
-    print(df)
-
-
-    #df = pyupbit.get_ohlcv(ticker, interval="minutes3", count=10)
+    print(f"CheckTicker:{ticker}")
+    listTickerSpike.clear()
+    df = pyupbit.get_ohlcv(ticker['market'], interval="minutes3", count=200)
     #print(df)
 
-    #df = pyupbit.get_ohlcv(ticker, interval="minutes3", count=1)
-    #print(df)
+    list_volume = df['volume'].tolist()
+    #print(list_volume)
+    #print(f"col_one_list count: {len(col_one_list)}")
 
-    col_one_list = df['volume'].tolist()
-    print(col_one_list)
-    print(f"col_one_list count: {len(col_one_list)}")
+    #앞 뒤 모두 OUTLIER_COUNT 수만큼 지움.
+    #col_one_list2 = col_one_list[OUTLIER_COUNT:len(col_one_list) - OUTLIER_COUNT]
+    #print(col_one_list2)
+    #print(f"col_one_list2 count: {len(col_one_list2)}")
 
-    col_one_list2 = col_one_list[OUTLIER_COUNT:len(col_one_list) - OUTLIER_COUNT]
-    print(col_one_list2)
-    print(f"col_one_list2 count: {len(col_one_list2)}")
+    #앞에만 OUTLIER_COUNT 수만큼 지움.
+    #col_one_list3 = col_one_list[OUTLIER_COUNT:len(col_one_list)]
+    #print(col_one_list3)
+    #print(f"col_one_list2 count: {len(col_one_list3)}")
 
-    prev_stdev_vol = stdev(col_one_list)
-    print(f"col_one_list stdev: {stdev(col_one_list)}")
-    print(f"col_one_list2 stdev: {stdev(col_one_list2)}")
+    #뒤에만 OUTLIER_COUNT 수만큼 지움.
+    list_volume_before = list_volume[0:len(list_volume) - OUTLIER_COUNT]
+    #print(col_one_list4)
+    #print(f"col_one_list2 count: {len(col_one_list4)}")
 
-CheckTicker('KRW-BTC')
+    stdev_volume_before = stdev(list_volume_before)
+    stdev_volume = stdev(list_volume)
+
+    if (stdev_volume_before * 2.0) < stdev_volume:
+        listTickerSpike.append(ticker)
+
+MakeTickerList(list_ticker, list_ticker_warning)
+
+for ticker in list_ticker:
+    CheckTicker(ticker, list_spike)
+
+print(f"list_spike : {list_spike}")
+    
+
+
 
 # 체크 시작
 '''
