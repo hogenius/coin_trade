e2e11082cfc4b4eea836d3868308ddc20230b8f0
hogenius <hogeniuss@gmail.com>
2024-03-26 23:03:19 +0900

    config security  divide

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ class ConfigInfo(SingletonInstane):
     def __init__(self):
         with open('config.yaml') as f:
             config_data = yaml.load(f, Loader=yaml.FullLoader)
-            self.access = config_data['key_access']
-            self.secret = config_data['key_secret']
             self.coin_name = config_data['coin_name']
             self.ma_1 = config_data['ma_line_1']
             self.ma_2 = config_data['ma_line_2']
@@ -14,7 +12,15 @@ class ConfigInfo(SingletonInstane):
             self.ma_check_sec = config_data['ma_check_sec']
             self.loop_sec = config_data['loop_sec']
             self.loop_check_sec = config_data['loop_check_sec']
-            self.discord_hook = config_data['discord_hook']
-            self.telegram_token = config_data['telegram_token']
-            self.telegram_chat_id = config_data['telegram_chat_id']
             self.list_coin = config_data['list_coin']
+        
+        with open('security.yaml') as s:
+            security_data = yaml.load(s, Loader=yaml.FullLoader)
+            self.access = security_data['key_access']
+            self.secret = security_data['key_secret']
+            self.discord_hook = security_data['discord_hook']
+            self.telegram_token = security_data['telegram_token']
+            self.telegram_chat_id = security_data['telegram_chat_id']
+
+        print(f"self.coin_name : {self.coin_name}")
+        print(f"self.access : {self.access}")
