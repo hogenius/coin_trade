187c1d8c6e6c74a221e0cdf22795b1f4e37d38b9
hogenius <hogeniuss@gmail.com>
2024-11-16 20:33:11 +0900

    환율 기능 수정

--- exchange_rate.py
+++ exchange_rate.py
@@ -4,13 +4,20 @@ import yfinance as yf
 class ExchangeRater(SingletonInstane):
 
     def GetUSD(self):
-        df = yf.download("KRW=X")
-        #print("현재 원/달러 환율:", df)
-        rate = df["Close"][-1]
 
+        try:
+            df = yf.download(tickers="KRW=X", period="1d", interval="1m")
+            #print("현재 원/달러 환율:", df)
+            #rate = df["Close"][-1]
+            rate = df["Close"].iloc[-1].item() 
+        except Exception as e:
+            rate = -1
+        
         return rate
-
-#ExchangeRater.Test()
+    
+if __name__ == '__main__':
+    exchangeRater = ExchangeRater()
+    print(f"test : {exchangeRater.GetUSD()}")
         
 
     
